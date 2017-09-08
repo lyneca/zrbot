ec42d77bc8e268959a6b05809ca7165ae9a94055
lyneca <lukemtuthill@gmail.com>
2017-09-08 22:29:24 +1000

    changed up port number

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from json import loads, dumps
 
 def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('', 8080)
+    server_address = ('', 80)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
